abc8abc9d1b2df2e3fbd4c951f6dccd8dc3514db
coapacetic <kyle.d.coapman@gmail.com>
2020-12-06 14:48:54 -0500

    update base testinfo models

---
+++ kilkenny-transform/models/staging/apcalcbc/testinfo/base/base_1718_q1_apcalcbc_testinfo.sql
@@ -0,0 +1,37 @@
+with source as (
+
+    select * from {{ source('apcalcbc', '1718_q1_apcalcbc_testinfo') }}
+
+),
+
+renamed as (
+
+    select
+        -- ids
+        question_number,
+
+        -- standards
+        question_type,
+        difficulty,
+        total_points,
+        answer_choices,
+        correct_answer,
+        primary_standard,
+        secondary_standard
+
+        -- ignored
+        -- year,
+        -- title,
+        -- subject_area,
+        -- low_end,
+        -- high_end,
+        -- grade_code,
+        -- grade,
+        -- document
+
+    from source
+    where question_number is not null
+
+)
+
+select * from renamed
