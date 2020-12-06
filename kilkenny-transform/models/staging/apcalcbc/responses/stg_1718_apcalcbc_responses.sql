d67dfae272de5a853b5564dfd4650b74b0a82223
coapacetic <kyle.d.coapman@gmail.com>
2020-12-06 14:50:47 -0500

    staging responses

---
+++ kilkenny-transform/models/staging/apcalcbc/responses/stg_1718_apcalcbc_responses.sql
@@ -0,0 +1,55 @@
+{{ config(
+    materialized='table'
+)}}
+
+with q1_responses as (
+    select * from {{ ref('unpivot_1718_q1_apcalcbc_responses')}}
+), 
+
+q2_responses as (
+    select * from {{ ref('unpivot_1718_q2_apcalcbc_responses')}}
+),
+
+q3_responses as (
+    select * from {{ ref('unpivot_1718_q3_apcalcbc_responses')}}
+),
+
+unioned as (
+
+    select
+        *,
+        'q1' as quarter,
+        '1718' as year
+    from q1_responses
+
+    union all
+
+    select
+        *,
+        'q2' as quarter,
+        '1718' as year
+    from q2_responses
+
+    union all
+    
+    select 
+        *,
+        'q3' as quarter,
+        '1718' as year
+    from q3_responses
+
+),
+
+final as (
+    select 
+        {{ dbt_utils.surrogate_key(['student_id','question','quarter','year'])}} as response_id,
+        student_id,
+        question,
+        quarter,
+        year,
+        response,
+
+    from unioned
+)
+
+select * from final
