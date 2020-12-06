2f466fb2b505013b8e479f144472ca1a93ec027e
coapacetic <kyle.d.coapman@gmail.com>
2020-12-06 14:50:34 -0500

    staging testinfo

---
+++ kilkenny-transform/models/staging/apcalcbc/testinfo/stg_1718_apcalcbc_testinfo.sql
@@ -0,0 +1,57 @@
+{{ config(
+    materialized='table'
+)}}
+
+with q1_testinfo as (
+    select * from {{ ref('base_1718_q1_apcalcbc_testinfo') }}
+),
+
+q2_testinfo as (
+    select * from {{ ref('base_1718_q2_apcalcbc_testinfo') }}
+),
+
+q3_testinfo as (
+    select * from {{ ref('base_1718_q3_apcalcbc_testinfo') }}
+),
+
+unioned as (
+    select
+        *,
+        'q1' as quarter,
+        '1718' as year
+    from q1_testinfo
+
+    union all
+
+    select 
+        *,
+        'q2' as quarter,
+        '1718' as year
+    from q2_testinfo
+
+    union all
+
+    select 
+        *,
+        'q3' as quarter,
+        '1718' as year
+    from q3_testinfo
+),
+
+final as (
+    select 
+        {{ dbt_utils.surrogate_key(['question_number','quarter','year'])}} as question_id,
+        question_number,
+        quarter,
+        year,
+        question_type,
+        difficulty,
+        total_points,
+        correct_answer,
+        primary_standard,
+        secondary_standard
+    from unioned
+    order by question_number
+)
+
+select * from final
